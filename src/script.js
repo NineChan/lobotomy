b6937ba2348414a07915397f70997b6d550aed5e
AlgorithmIntensity <circle.2025@mail.ru>
2026-04-04 14:41:11 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -5,13 +5,13 @@ let rand = Math.random() * 360;
 
 gif1.addEventListener('click', (event) => {
     for (let i = 0; i < 1488; i++) {
-        gif1.style.transform = `rotate(${randomDeg}deg) scale(${randomScale})`;
+        gif1.style.transform = `rotate(${random}deg) scale(${random})`;
     }
 });
 
 // Детектим клик на втором элементе
 gif2.addEventListener('click', (event) => {
     for (let i = 0; i < 1488; i++) {
-        gif2.style.transform = `rotate(${randomDeg}deg) scale(${randomScale})`;
+        gif2.style.transform = `rotate(${random}deg) scale(${random})`;
     }
 });
